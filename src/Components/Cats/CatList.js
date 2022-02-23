9ed593ce95e4c8b38d91cbf4d2f6bf240e1254a8
allenbra17 <allenbra17@gmail.com>
2022-02-23 14:56:37 -0500

    finsihed cats

--- src/Components/Cats/CatList.js
+++ src/Components/Cats/CatList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
- 
-const CatList = () => {
- return (
-   <div>
-     {props.breeds.Map(cat => <li>cat</li> )}
-   </div>
- )
+class CatList extends Component{
+  render() {
+    const displayCats = () => {
+      return this.props.cats.map(cat => <li>{cat}</li>)
+    }
+    return (
+      <div>
+        <ul>{displayCats()}</ul>
+      </div>
+    )
+  }
 }
- 
-CatList;
+export default CatList;
